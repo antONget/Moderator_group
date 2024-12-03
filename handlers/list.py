e4957f914cefce29a7e35c63f2b1c8f5cdb562ab
antONget <a.l.ponomare@mail.ru>
2024-12-03 23:09:33 +0300

    8 правки

--- handlers/list.py
+++ handlers/list.py
@@ -25,29 +25,32 @@ async def into_command_list(message: Message, bot: Bot) -> None:
 
     users = await rq.get_users()
     group = await rq.get_groups_group_id(group_id=message.chat.id)
-    groups = await rq.get_groups()
-    text = ''
-    i = 0
+    # groups = await rq.get_groups()
+    # text = ''
+    # i = 0
     # await bot.send_message(chat_id=config.tg_bot.support_id,
     #                        text='list')
     if group.group_clan == 'general':
         # await bot.send_message(chat_id=config.tg_bot.support_id,
         #                        text='general')
-        for group_ in groups:
+        text = 'УЧАСТНИКИ КЛАНА:'
+        # for group_ in groups:
             # text = f'<b>{group_.group_id}</b>\n\n'
-            text = ''
-            i = 0
-            for user in users:
-                member = await bot.get_chat_member(user_id=user.tg_id,
-                                                   chat_id=group_.group_id)
-                if member.status not in ['left', 'kicked']:
-                    if user.nickname:
-                        i += 1
-                        text += f'{i}. <a href="tg://user?id={user.tg_id}">{user.nickname}</a>\n'
+            # text = ''
+        i = 0
+        for user in users:
+            member = await bot.get_chat_member(user_id=user.tg_id,
+                                               chat_id=message.chat.id)
+            if member.status not in ['left', 'kicked']:
+                if user.nickname:
+                    i += 1
+                    text += f'{i}. <a href="tg://user?id={user.tg_id}">{user.nickname}</a>\n'
             # await bot.send_message(chat_id=config.tg_bot.support_id,
             #                        text=text)
-            await message.answer(text=text)
+        await message.answer(text=text)
     else:
+        text = f'{message.chat.title}'
+        i = 0
         for user in users:
             member = await bot.get_chat_member(user_id=user.tg_id,
                                                chat_id=message.chat.id)
